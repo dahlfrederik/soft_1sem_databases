60827df6aaffdbbe110075cd57449c41fd4e6242
josefmarcc <josefmarcc@gmail.com>
2022-02-23 16:34:58 +0100

    Update createDBscript.sql

--- Handin2/createDBscript.sql
+++ Handin2/createDBscript.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Persons (
-	CPR varchar(10) PRIMARY KEY,
+	CPR varchar(10) PRIMARY KEY NOT NULL,
 	first_name varchar(255),
 	last_name varchar(255),
 	gender varchar(6),
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS Persons (
 );
 
 CREATE TABLE IF NOT EXISTS Doctors (
-	CPR varchar PRIMARY KEY,
+	CPR varchar PRIMARY KEY NOT NULL,
 	occupation varchar(255),
 	constraint CPR
 	foreign key (CPR)
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Doctors (
 
 
 CREATE TABLE IF NOT EXISTS Patients (
-	CPR varchar PRIMARY KEY,
+	CPR varchar PRIMARY KEY NOT NULL,
 	description varchar(255),
 	constraint CPR
 	foreign key (CPR)
@@ -25,9 +25,9 @@ CREATE TABLE IF NOT EXISTS Patients (
 );
 
 CREATE TABLE IF NOT EXISTS Prescription (
-	prescription_id SERIAL PRIMARY KEY,
-	patient_id varchar,
-	doctor_id varchar,
+	prescription_id SERIAL PRIMARY KEY NOT NULL,
+	patient_id varchar NOT NULL,
+	doctor_id varchar NOT NULL,
 	description varchar(255),
 	medicin_name varchar(255),
 	handouts int,
@@ -41,13 +41,13 @@ CREATE TABLE IF NOT EXISTS Prescription (
 );
 
 CREATE TABLE IF NOT EXISTS Cities (
-	postal_code int PRIMARY KEY,
+	postal_code int PRIMARY KEY NOT NULL,
 	city varchar(255)
 );
 
 CREATE TABLE IF NOT EXISTS Address (
-	address_id SERIAL PRIMARY KEY,
-	postal_code int,
+	address_id SERIAL PRIMARY KEY NOT NULL,
+	postal_code int NOT NULL,
 	street varchar(255),
 	house_number varchar(255),
 	constraint postal_code 
@@ -56,19 +56,19 @@ CREATE TABLE IF NOT EXISTS Address (
 );
 
 CREATE TABLE IF NOT EXISTS Pharmacy (
-	pharmacy_id SERIAL PRIMARY KEY,
-	pharmacy_name varchar(255),
-	address_id int,
+	pharmacy_id SERIAL PRIMARY KEY NOT NULL,
+	pharmacy_name varchar(255) NOT NULL,
+	address_id int NOT NULL,
 	constraint address_id 
 	foreign key (address_id)
 		references Address(address_id)
 );
 
 CREATE TABLE IF NOT EXISTS Delivered (
-	delivered_id SERIAL PRIMARY KEY,
-	pharmacy_id int,
-	prescription_id int,
-	delivered_date timestamp,
+	delivered_id SERIAL PRIMARY KEY NOT NULL,
+	pharmacy_id int NOT NULL,
+	prescription_id int NOT NULL,
+	delivered_date timestamp NOT NULL,
 	constraint pharmacy_id 
 	foreign key (pharmacy_id)
 		references Pharmacy(pharmacy_id),
@@ -76,3 +76,4 @@ CREATE TABLE IF NOT EXISTS Delivered (
 	foreign key (prescription_id)
 		references Prescription(prescription_id)
 );
+
