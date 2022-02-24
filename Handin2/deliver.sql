53fb0b4d267a4b70b7c2fb733407a41bd0834f5e
Sebastian <cph-sb287@cphbusiness.dk>
2022-02-24 12:49:25 +0100

    deliver.sql updated

--- Handin2/deliver.sql
+++ Handin2/deliver.sql
@@ -1,45 +1,45 @@
-CREATE TABLE IF NOT EXISTS prescription (
-  id SERIAL primary key,
-  patient_id int NOT null,
-  doctor_id int NOT null,
-  description varchar(255)NOT null,
-  medicin_name varchar(255) NOT null,
-  handouts int NOT null,
-  expire_date date NOT null
-);
+--Creating what is needed 
+INSERT INTO persons(CPR, first_name,last_name,gender,phone, email) 
+VALUES ('010100001', 'John', 'Patient Doe', 'male', 123456789, 'something@something.com');
+INSERT INTO persons(CPR, first_name,last_name,gender,phone, email) 
+VALUES ('020200001', 'John', 'Doctor Doe', 'male', 123456799, 'somethingdoctor@something.com');
+INSERT INTO doctors (CPR,occupation) 
+VALUES ('020200001', 'mystery department');
+INSERT INTO patients  (CPR,description) 
+VALUES ('010100001', 'Special description about whatever');
+INSERT INTO cities(postal_code, city) values (2500, 'Herlev');
+INSERT INTO address(postal_code, street, house_number) VALUES (2500, 'herlev hovedgade', 1);
+INSERT INTO pharmacy(pharmacy_name, address_id) VALUES ('PharmacyHerlev', 2);
+-- Displaying it 
+SELECT * FROM persons;
+SELECT * FROM doctors ;
+SELECT * FROM patients ;
+SELECT * FROM prescription p ;
+SELECT * FROM address a ;
+SELECT * FROM pharmacy p ;
+-- Calling the function 
+SELECT create_prescription('010100001', '020200001', 'Fever', 'Panodil', 4, '2022-02-23 14:01:10-08');
+SELECT * FROM prescription;
 
-CREATE TABLE if not exists delivered (
-	delivered_id SERIAL primary key,
-	delivered_date date NOT null,
-	prescription_id int NOT null,
-	CONSTRAINT prescription_id 
-	foreign key (prescription_id)
-		references prescription(id)
-);
+DROP FUNCTION deliverPrescription(pat_id varchar, med_name varchar, phar_name varchar);
 
-
-
-INSERT INTO	prescription(patient_id, doctor_id, description, medicin_name, handouts, expire_date)
-	VALUES (1, 1, 'for broken pp', 'pp pills', 10, '2022-12-01');
-	
-SELECT * FROM prescription p;
-SELECT * FROM delivered d;
-INSERT INTO delivered(delivered_date, prescription_id) VALUES (NOW(), 1); 
-
-
-CREATE OR REPLACE FUNCTION deliverPrescription(pat_id int, med_name varchar)
+CREATE OR REPLACE FUNCTION deliverPrescription(pat_id varchar, med_name varchar, phar_name varchar)
 RETURNS VOID AS
 $$
 DECLARE 
 	pre_id  int;
+	phar_id int;
 BEGIN
-	SELECT id FROM prescription WHERE patient_id = pat_id AND medicin_name = med_name INTO pre_id;
+	SELECT prescription_id FROM prescription WHERE patient_id = pat_id AND medicin_name = med_name INTO pre_id;
+	SELECT pharmacy_id FROM pharmacy p WHERE p.pharmacy_name = phar_name INTO phar_id;
 	UPDATE prescription SET handouts = handouts - 1 WHERE patient_id = pat_id;	
-    INSERT INTO delivered(delivered_date, prescription_id) VALUES (NOW(), pre_id); 
+    INSERT INTO delivered(delivered_date, prescription_id, pharmacy_id) VALUES (NOW(), pre_id, phar_id); 
 END
 $$
 LANGUAGE 'plpgsql';
  
- 
- 
-SELECT deliverPrescription(1, 'pp pills');
+SELECT deliverPrescription('010100001', 'Panodil', 'PharmacyHerlev');
+
+SELECT * FROM prescription p  ;
+
+
