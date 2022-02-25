7490e1cb368b3a20b8ccf10c020c95d660f20487
Frederik Dinsen <gammarik@gmail.com>
2022-02-25 10:56:09 +0100

    Adds set_doctor_or_patient() function

--- Handin2/inserting-people-into-table.sql
+++ Handin2/inserting-people-into-table.sql
@@ -4,3 +4,4 @@ INSERT INTO tempperson (cpr, firstname, lastname, gender, birthday, phone, email
 SELECT p.cpr, p.firstname, p.lastname, p.gender, p.birthday, p.phone, p.email
 FROM generate_people(1000) AS p;
 
+PERFORM set_doctor_or_patient();
