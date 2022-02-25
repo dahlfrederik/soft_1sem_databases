ceb32ccf8f5a558c0465f59840afe66dc0b6ac3a
Frederik Dinsen <gammarik@gmail.com>
2022-02-25 10:57:09 +0100

    removes unused create table

--- Handin2/inserting-people-into-table.sql
+++ Handin2/inserting-people-into-table.sql
@@ -1,5 +1,3 @@
-CREATE TABLE tempperson(cpr TEXT PRIMARY KEY, firstname TEXT, lastname TEXT, gender TEXT, birthday timestamp, phone TEXT, email TEXT);
-
 INSERT INTO tempperson (cpr, firstname, lastname, gender, birthday, phone, email)
 SELECT p.cpr, p.firstname, p.lastname, p.gender, p.birthday, p.phone, p.email
 FROM generate_people(1000) AS p;
